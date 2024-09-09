0cb5de6afd0a55a5282efafcf8898b9bc090f4f7
delairs <delair27rs@gmail.com>
2024-09-09 18:28:08 +0700

    first commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,25 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <h3 class="text-xl">Ini Blog Saya!</h3>
+</x-layout>
+
+
+{{-- 
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+</head>
+<body>
+    <h1>Ini artikel</h1>
+    <article>Tes</article>
+    <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Dolor, excepturi repellat! Ipsam doloremque temporibus corrupti 
+        nisi harum quasi, error soluta eaque quo ab repellat, vitae cum voluptas itaque, alias debitis.</h3>
+</body>
+</html> --}}
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,22 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <h3 class="text-xl">Ini Contact!</h3>
+</x-layout>
+{{-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Contact</title>
+</head>
+<body>
+    <ul>
+        <li class="text-3xl font-bold underline"> Instagram   : @delai_stywn </li>
+        <li class="text-3xl font-bold underline"> Line        : @delaiaja </li>
+        <li class="text-3xl font-bold underline"> Twitter     : @lildice27</li>
+        
+    </ul>
+</body>
+</html> --}}
