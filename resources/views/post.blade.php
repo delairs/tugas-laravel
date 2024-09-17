225e6eadf10f7078fabc4a4677c8d39f81d41426
delairs <delair27rs@gmail.com>
2024-09-17 22:47:20 +0700

    View Data

---
+++ resources/views/post.blade.php
@@ -0,0 +1,36 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <article class="py-10 max-w-screen-md border-b border-black-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold">{{ $post['title'] }}</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">{{ $post['author'] }}</a> | 27 September 2004
+        </div>
+        <p class="my-4 font-light">
+            {{ $post['body'] }}
+        </p>
+        <a href="/posts" class="font-medium text-blue-500 hover:uppercase">&laquo; Back to Posts</a>
+    </article>
+</x-layout>
+
+
+
+
+{{-- 
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+</head>
+<body>
+    <h1>Ini artikel</h1>
+    <article>Tes</article>
+    <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Dolor, excepturi repellat! Ipsam doloremque temporibus corrupti 
+        nisi harum quasi, error soluta eaque quo ab repellat, vitae cum voluptas itaque, alias debitis.</h3>
+</body>
+</html> --}}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,44 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts/{id}', function($id){
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel 1',
+            'author' => 'Delai R. S.',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, ratione voluptas dicta tempora ea libero minima nisi, eius cum labore quis suscipit, fugiat ullam accusantium quibusdam sint pariatur dolores atque.'  
+        ],        
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'author' => 'Delai R. S.',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, ratione voluptas dicta tempora ea libero minima nisi, eius cum labore quis suscipit, fugiat ullam accusantium quibusdam sint pariatur dolores atque.'       
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use ($id){
+        return $post['id'] == $id;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+    [
+        'id' => 1,
+        'title' => 'Judul Artikel 1',
+        'author' => 'Delai R. S.',
+        'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, ratione voluptas dicta tempora ea libero minima nisi, eius cum labore quis suscipit, fugiat ullam accusantium quibusdam sint pariatur dolores atque.'  
+    ],        
+    [
+        'id' => 2,
+        'title' => 'Judul Artikel 2',
+        'author' => 'Delai R. S.',
+        'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, ratione voluptas dicta tempora ea libero minima nisi, eius cum labore quis suscipit, fugiat ullam accusantium quibusdam sint pariatur dolores atque.'       
+    ]
+    ]]);
 });
 
 Route::get('/contact', function () {
