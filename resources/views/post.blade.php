4bf0d8f549e24f03b6d8b1d4ff593b995e260965
delairs <delair27rs@gmail.com>
2024-10-12 20:10:12 +0700

    Post Category

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
     <article class="py-10 max-w-screen-md border-b border-black-300">
         <h2 class="mb-1 text-3xl tracking-tight font-bold">{{ $post['title'] }}</h2>
         <div class="text-base text-gray-500">
-            <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | {{ $post['created_at']->format('j F Y') }} ({{ $post['created_at']->diffForHumans() }})
+            <a href="/authors/{{ $post->author->username }}" class="hover:underline">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="hover:underline">{{ $post->category->name }}</a> | {{ $post['created_at']->format('j F Y') }} ({{ $post['created_at']->diffForHumans() }})
         </div>
         <p class="my-4 font-light">
             {{ $post['body'] }}
